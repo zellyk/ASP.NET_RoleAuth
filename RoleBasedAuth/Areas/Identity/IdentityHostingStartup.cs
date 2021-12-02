5079d472d958b7bc0788125af8a6332281d88249
Jonathan Ma <jonathan.marcotte.7@gmail.com>
2021-12-02 17:45:37 -0500

    first commit

---
+++ RoleBasedAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoleBasedAuth.Data;
+
+[assembly: HostingStartup(typeof(RoleBasedAuth.Areas.Identity.IdentityHostingStartup))]
+namespace RoleBasedAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
